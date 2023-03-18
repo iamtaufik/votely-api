49e34c71060d8f197cf92ac2bd02fdb4df9fc365
iamtaufik <muhtopik07@gmail.com>
2023-03-18 23:17:42 +0700

    fix votes controller

--- middlewares/Auth.middleware.ts
+++ middlewares/Auth.middleware.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 
 const verivyAuth = async (req: Request, res: Response, next: NextFunction) => {
-  if (!req.user) return res.status(400).json({ status: 'UNAUTHORIZED', code: 400 });
+  if (req.user === undefined) return res.status(400).json({ status: 'UNAUTHORIZED', code: 400 });
   next();
 };
 
